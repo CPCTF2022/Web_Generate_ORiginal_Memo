e26df84e1df2fe6d557145ac6b7d4ba7b5e8098f
mazrean <shunsuke.w04@gmail.com>
2022-04-30 11:56:37 +0900

    fix client name

--- client/src/components/Nav.svelte
+++ client/src/components/Nav.svelte
@@ -1,37 +1,37 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
-  import Button, { Label } from "@smui/button";
-  import type { User } from "../domain/user";
-  export let path: string;
-  export let user: User|null;
+	import { goto } from '$app/navigation';
+	import Button, { Label } from '@smui/button';
+	import type { User } from '../domain/user';
+	export let path: string;
+	export let user: User | null;
 </script>
 
 <nav>
-  <div class="mdc-typography--headline2 title">Generate ORiginal Memo 1</div>
-  {#if !user}
-    <div style="text-align: right;">
-      {#if path !== "login"}
-        <Button on:click={() => goto("/login")}>
-          <Label>Login</Label>
-        </Button>
-      {/if}
-      {#if path !== "signup"}
-        <Button on:click={() => goto("/signup")}>
-          <Label>Signup</Label>
-        </Button>
-      {/if}
-    </div>
-  {/if}
+	<div class="mdc-typography--headline2 title">Generate ORiginal Memo</div>
+	{#if !user}
+		<div style="text-align: right;">
+			{#if path !== 'login'}
+				<Button on:click={() => goto('/login')}>
+					<Label>Login</Label>
+				</Button>
+			{/if}
+			{#if path !== 'signup'}
+				<Button on:click={() => goto('/signup')}>
+					<Label>Signup</Label>
+				</Button>
+			{/if}
+		</div>
+	{/if}
 </nav>
 
 <style>
-  nav {
-    border-bottom: 1px solid rgba(255, 62, 0, 0.1);
-    font-weight: 300;
-    padding: 0 1em;
-  }
-  .title {
-    margin: 0 auto;
-    text-align: center;
-  }
+	nav {
+		border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+		font-weight: 300;
+		padding: 0 1em;
+	}
+	.title {
+		margin: 0 auto;
+		text-align: center;
+	}
 </style>
